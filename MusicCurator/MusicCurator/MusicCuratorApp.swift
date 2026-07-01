408cfc3e264a7f3e8395fb776d9d6f08d116e444
Wolfgang Dufek <wdufek@gmail.con>
2026-07-01 17:06:21 +0200

    Initial SwiftUI project

---
+++ MusicCurator/MusicCurator/MusicCuratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicCuratorApp.swift
+//  MusicCurator
+//
+//  Created by Wolfgang on 01.07.26.
+//
+
+import SwiftUI
+
+@main
+struct MusicCuratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
